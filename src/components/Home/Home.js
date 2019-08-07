b9cd9ad724d006d103b5bcdcb5c4660b39078500
Woof <ts00140421@gmail.com>
2019-08-07 18:12:10 +0800

    Add recommend channels and games to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import ChannelContainer from '../Channel/ChannelContainer'
+import DirectoryContainer from '../Directory/DirectoryContainer';
 
 const HomeDiv = styled.div`
     display: flex;
@@ -8,12 +10,69 @@ const HomeDiv = styled.div`
     align-items: center;
     justify-content: center;
 `;
+
+const RecommendLiveDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 35vw;
+    @media screen and (max-width:800px) {
+         height: 56.25vw;
+    }
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 2vmax;
+`;
+const RecommendDiv = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const GameDiv = styled.div`    
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+const ChannelDiv = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+const TitleDiv = styled.div`
+    display: block;
+    font-size: 2em;
+    margin: 0;
+    width: 90vw;
+    text-align: center;
+    border-top: solid 0.05em white;
+    color: white;
+`;
+
 class Home extends React.Component {
     render() {
-
+        const {
+            recommendClassNum,
+            recommendTopNum,
+        } = this.props;
         return (
             <HomeDiv>
-            home
+                <RecommendDiv>
+                    <GameDiv>
+                        <TitleDiv>推薦的分類</TitleDiv>
+                        <DirectoryContainer buttonSet={false} range={recommendClassNum}/>
+                    </GameDiv>
+                    <ChannelDiv>
+                        <TitleDiv>推薦的頻道</TitleDiv>
+                        <ChannelContainer buttonSet={false} range={recommendTopNum}/>
+                    </ChannelDiv>
+                </RecommendDiv>
             </HomeDiv>
         );
     }
--- src/components/Home/HomeContainer.js
+++ src/components/Home/HomeContainer.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Home from './Home';
+
+const CLASS_LIMIT = 6;
+const TOP_LIMIT = 8;
 class HomeContainer extends React.Component{
     render(){
         return(
-            <Home />
+            <Home
+                recommendClassNum={CLASS_LIMIT}
+                recommendTopNum={TOP_LIMIT}
+            />
         );
     }
 }
